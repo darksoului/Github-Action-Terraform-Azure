5531c84d0e9a001bdbb654f3e24f87222ec369ac
Mr.Venkat_Develops <118845171+darksoului@users.noreply.github.com>
2024-02-14 14:55:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
+    resource_group_name  = "VenkatGit"
+    storage_account_name = "terraformvenkat"
+    container_name       = "csvenkat"
     key                  = "dev.terraform.tfstate"
   }
 }
